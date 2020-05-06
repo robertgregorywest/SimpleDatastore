5477b58a19d95c6cef8596abe37f1e4d91fd45c6
Rob West <robertgregorywest@gmail.com>
2020-05-06 18:46:03 +0100

    Tweaking AssemblyInfo

--- SimpleDatastore/Properties/AssemblyInfo.cs
+++ SimpleDatastore/Properties/AssemblyInfo.cs
@@ -1,12 +1,8 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyCompany("Rob West")]
-[assembly: AssemblyDescription("A simple .NET persistence library which uses XML file storage")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: AssemblyInformationalVersion("1.0.0")]
-[assembly: AssemblyProduct("SimpleDatastore")]
-[assembly: AssemblyTitle("SimpleDatastore")]
 [assembly: AssemblyVersion("1.0.0.0")]
 
 [assembly: InternalsVisibleTo("SimpleDatastore.Tests")]
